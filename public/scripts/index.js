31e53d3ba886211c523eae565ecee2acbf0706c9
brightboost <info@brightboost.nl>
2022-08-25 16:21:15 +0200

    finish project

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -24,12 +24,33 @@ function populateDropdown(stateList) {
   selectState.appendChild(selectStateDropdown);
 
   let stateListLength = stateList.length;
-  for(let i = 0; i < stateListLength; i++) {
-    let stateListOption = new Option(stateList[i].name, stateList[i].code);
+  for (let i = 0; i < stateListLength; i++) {
+    let stateListOption = new Option(stateList[i].name, stateList[i].name);
     selectState.appendChild(stateListOption);
   }
 }
 
 function onChangeDisplayCity() {
-  
+  let selectedState = document.getElementById("selectState").value;
+  let url = `/api/cities/${selectedState}`;
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let cities = JSON.parse(this.responseText);
+      createCitiesTable(cities);
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send(null);
+}
+
+function createCitiesTable(cities) {
+  let cityTableBody = document.getElementById("cityTableBody");
+  cityTableBody.innerHTML = "";
+  let length = cities.length;
+  for (let i = 0; i < length; i++) {
+    let row = cityTableBody.insertRow(-1);
+    let cell0 = row.insertCell(0);
+    cell0.innerHTML = cities[i].city;
+  }
 }
